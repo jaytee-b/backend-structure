3a8a9bf3d3f40dbc880251da9313cb6107d737eb
Jesutoni Adeleye <jesutoniadeleye@gmail.com>
2024-10-07 16:20:34 +0100

    updated code no error

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { createOrder } = require("../controller/orderController");
+const { createOrder } = require("../controllers/orderController");
 
 const router = express.Router()
 
 
-router.post('/createOrder', createOrder)
+router.post("/createOrder", createOrder)
 
 
 module.exports = router
